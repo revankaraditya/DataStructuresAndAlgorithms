8f33f920a9219eb0f50dba4e15e25de0f3d16684
Aditya0Revankar <aditya0revankar@gmail.com>
2024-06-05 11:01:51 +0530

    BST

--- BinaryTree/src/Tree.java
+++ BinaryTree/src/Tree.java
@@ -3,53 +3,44 @@ public class Tree {
         private int value;
         private Node leftChild;
         private Node rightChild;
-        private int count;
         Node(int value){
             this.value = value;
         }
+        @Override
+        public String toString(){
+            return "Node: " + value;
+        }
     }
     private Node root = null;
-
     void insert(int value){
         if(root==null){
             root = new Node(value);
-            root.leftChild=null;
-            root.rightChild=null;
+            return;
         }
-        else{
-            var current = root;
-            Node node = new Node(value);
-            while(true) {
-                if(current.value==value){
-                    current.count++;
+        var current = root;
+        Node newNode = new Node(value);
+        while(true) {
+            if (current.value == value) {
+                throw new IllegalStateException(value + " Already exists");
+            } else if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
                     break;
                 }
-                else if (value < current.value) {
-                    if(current.leftChild==null) {
-                        current.leftChild = node;
-                        break;
-                    }
-                    current=current.leftChild;
+                current = current.leftChild;
+            } else if (value > current.value) {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
+                    break;
                 }
-                else if(value > current.value) {
-                    if(current.rightChild==null){
-                        current.rightChild = node;
-                        break;
-                    }
-                    current=current.rightChild;
-                }
+                current = current.rightChild;
             }
         }
     }
     boolean find(int value){
         var current = root;
-        if(root==null){
-            throw new IllegalStateException("BST Empty");
-        }
-        while(true){
-            if(current.value==value)
-                return true;
-            else if (value < current.value) {
+        while(current!=null){
+            if (value < current.value) {
                 if(current.leftChild==null)
                     break;
                 current=current.leftChild;
@@ -58,6 +49,8 @@ public class Tree {
                     break;
                 current=current.rightChild;
             }
+            else //equals
+                return true;
         }
         return false;
     }
